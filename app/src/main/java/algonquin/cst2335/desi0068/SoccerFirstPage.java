fbd174e705a75894a88cb4d6d185b718304d48f9
AyeshDeSilva <77420272+AyeshDeSilva@users.noreply.github.com>
2021-07-13 11:11:42 -0400

    Starting app

---
+++ app/src/main/java/algonquin/cst2335/desi0068/SoccerFirstPage.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.desi0068;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoccerFirstPage extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soccer_firstpage);
+    }
+}
+
+
